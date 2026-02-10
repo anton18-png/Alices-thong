983d878f314cf2b1ed7f04edddf2e8fdb308478f
Scidonchik <86909941+anton18-png@users.noreply.github.com>
2026-02-10 17:38:49 +0300

    ⚜️ Add user data backup

--- code/install.bat
+++ code/install.bat
@@ -15,5 +15,18 @@ if not exist "%destFolder%" (
 rem Копируем все файлы и папки из исходной в целевую
 xcopy "%sourceFolder%\*" "%destFolder%\" /S /E /Y /H
 
+rem Теперь копируем папки из User Data
+set "userDataFolder=%USERPROFILE%\AppData\Local\Yandex\YandexBrowser\Application\User Data"
+set "destUserData=C:\Apps\Alices-Thong\user_data"
+
+rem Создаем папку назначения для User Data, если не существует
+if not exist "%destUserData%" (
+    mkdir "%destUserData%"
+)
+
+rem Копируем все содержимое из User Data в новую папку
+xcopy "%userDataFolder%\*" "%destUserData%\" /S /E /Y /H
+
 rem Запускаем файл remove-yandex.exe из папки назначения
 start "" "C:\Apps\Alices-Thong\remove-yandex.exe"
+
